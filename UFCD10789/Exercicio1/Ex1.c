80eb867f58a5b8827b67d2c28faa27b52e64c6c8
Suellen Almeida <suellen.almeida.t0126461@edu.atec.pt>
2025-02-20 11:13:50 +0000

    Exe1 Complete

--- UFCD10789/Exercicio1/Ex1.c
+++ UFCD10789/Exercicio1/Ex1.c
@@ -2,12 +2,21 @@
 
 int main(){
 
-    int Num = 0;
+    int Segundos;
 
-    for(int i = 0; i < 10; i++){
-        Num += 2;
-        printf("Num = %d, i = %d\n", Num, i);
-    }
+    //Recolha do tempo
+    printf("Insira o tempo em segundos: ");
+    scanf("%d", &Segundos);
+
+    //Conversão em minutos
+    float Minutos = (float) Segundos/60;
+
+    printf("%d segundo(s) em minutos é: %.2f minuto(s).\n", Segundos, Minutos);
+
+    //Conversão em horas
+    float Horas = Minutos/60;
+
+    printf("%d Segundos(s) em Horas é: %.4f Hora(s).", Segundos, Horas);
 
     return 0;
 
