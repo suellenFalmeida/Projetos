a6ae05e8e994abf8607c6c656c4d903b1df904b3
Suellen Almeida <suellen.almeida.t0126461@edu.atec.pt>
2025-02-25 15:03:14 +0000

    Exe6 Adicionar

---
+++ UFCD10789/Exercicio6/Ex6.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+
+    char nomeCliente[50];
+    float compra, desconto, valorFinal;
+    int porcentagem;
+
+    printf("Nome do cliente: ");
+    scanf("%s", nomeCliente);
+
+    printf("Insira o valor da compra efetuada: ");
+    scanf("%f", &compra);
+
+    if (compra <= 200.00) {
+        porcentagem = 10;
+    } else if (compra <= 500.00) {
+        porcentagem = 15;
+    } else {
+        porcentagem = 20;
+    }
+
+    desconto = (porcentagem / 100.0) * compra;
+    valorFinal = compra - desconto;
+
+    printf("\nNome do cliente: %s\n", nomeCliente);
+    printf("Valor da compra: %.2f euros\n", compra);
+    printf("Percentual de desconto: %d%%\n", porcentagem);
+    printf("Valor do desconto: %.2f euros\n", desconto);
+    printf("Valor total a pagar: %.2f euros\n", valorFinal);
+
+}
