ea2a856953ca13e79706ade85cc969ab0dfeba51
Suellen Almeida <suellen.almeida.t0126461@edu.atec.pt>
2025-02-25 14:35:31 +0000

    Exe5 complete

---
+++ UFCD10789/Exercicio5/Exe5.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+int main(){
+
+    int num1, num2, num3;
+
+    printf("Insira o Primeiro valor: ");
+    scanf("%d", &num1);
+    printf("\nInsira o Segundo valor: ");
+    scanf("%d", &num2);
+    printf("\nInsira o Terceiro valor: ");
+    scanf("%d", &num3);
+
+    printf("Ordem crescente: ");
+    if (num1 <= num2 && num1 <= num3) {
+        printf("%d ", num1);
+        if (num2 <= num3) {
+            printf("%d %d\n", num2, num3);
+        } else {
+            printf("%d %d\n", num3, num2);
+        }
+    } else if (num2 <= num1 && num2 <= num3) {
+        printf("%d ", num2);
+        if (num1 <= num3) {
+            printf("%d %d\n", num1, num3);
+        } else {
+            printf("%d %d\n", num3, num1);
+        }
+    } else {
+        printf("%d ", num3);
+        if (num1 <= num2) {
+            printf("%d %d\n", num1, num2);
+        } else {
+            printf("%d %d\n", num2, num1);
+        }
+    }
+
+    printf("Ordem decrescente: ");
+    if (num1 >= num2 && num1 >= num3) {
+        printf("%d ", num1);
+        if (num2 >= num3) {
+            printf("%d %d\n", num2, num3);
+        } else {
+            printf("%d %d\n", num3, num2);
+        }
+    } else if (num2 >= num1 && num2 >= num3) {
+        printf("%d ", num2);
+        if (num1 >= num3) {
+            printf("%d %d\n", num1, num3);
+        } else {
+            printf("%d %d\n", num3, num1);
+        }
+    } else {
+        printf("%d ", num3);
+        if (num1 >= num2) {
+            printf("%d %d\n", num1, num2);
+        } else {
+            printf("%d %d\n", num2, num1);
+        }
+    }
+
+
+}
