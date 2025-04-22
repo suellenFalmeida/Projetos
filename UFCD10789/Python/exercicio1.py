2ad4eaf7d2583ff1b478929daf4391cdafaa4a46
Suellen Almeida <suellen.almeida.t0126461@edu.atec.pt>
2025-04-22 14:48:57 +0100

    Loop 1

--- UFCD10789/Python/exercicio1.py
+++ UFCD10789/Python/exercicio1.py
@@ -1,2 +1,10 @@
-# Crie um algoritmo que mostre os 30 primeiros números ímpares e pares.
+pares = []
+impares = []
 
+for i in range(30):
+    pares.append(i * 2)
+    impares.append(i * 2 + 1)
+
+print("Primeiros 30 números pares:", pares)
+
+print("Primeiros 30 números ímpares:", impares)
