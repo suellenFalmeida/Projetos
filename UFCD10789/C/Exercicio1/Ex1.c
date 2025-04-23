0eb7c56a06085c9abdd81ad42ae790e019b9d929
suellenFalmeida <suellen.almeida.t0126461@edu.atec.pt>
2025-04-23 19:40:05 +0100

    Update Ex1.c

--- UFCD10789/C/Exercicio1/Ex1.c
+++ UFCD10789/C/Exercicio1/Ex1.c
@@ -2,16 +2,17 @@
 
 int main(){
 
-    int Segundos;
+    int Segundos = 0;
+    int Horas = 0;
+    int minutosRestantes = 0;
+    int segundosRestantes = 0;
 
-    //Recolha do tempo
     printf("Insira o tempo em segundos: ");
     scanf("%d", &Segundos);
 
-    //Conversão em horas, minutos e segundos
-    int Horas = Segundos/3600;
-    int minutosRestantes = (Segundos % 3600) / 60;
-    int segundosRestantes = Segundos % 60;
+    Horas = Segundos/3600;
+    minutosRestantes = (Segundos % 3600) / 60;
+    segundosRestantes = Segundos % 60;
 
     printf("%d Segundos(s) é: %d Hora(s), %d minuto(s) e %d segundo(s)\n", Segundos, Horas, minutosRestantes, segundosRestantes);
 
