f01176816aed23e44bb00845d869414b8dfe60a8
Suellen Almeida <suellen.almeida.t0126461@edu.atec.pt>
2025-04-24 08:04:01 +0100

    atualizar

--- UFCD10789/Python/Exercicio19.py
+++ UFCD10789/Python/Exercicio19.py
@@ -1,14 +1,13 @@
 count = 0
 bonatchi = [1, 1]
+index = 0
 
 while count < 60:
     if count != 0 and count != 1:
         bonatchi.append(bonatchi[count - 1] + bonatchi[count - 2])
     count += 1
 
-idx = 0
-
 for n in bonatchi:
-    if idx != 0 and idx != 1:
-        print(f"{bonatchi[idx - 2]} + {bonatchi[idx - 1]} = {bonatchi[idx]}")
-    idx += 1
+    if index != 0 and index != 1:
+        print(f"{bonatchi[index - 2]} + {bonatchi[index - 1]} = {bonatchi[index]}")
+    index += 1
